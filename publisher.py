db13f9f49ebc688afce4afb2d8211f107f6f470d
JolanEggers <126364107+JolanEggers@users.noreply.github.com>
2025-04-22 10:19:44 +0200

    IP changed to localhost

--- publisher.py
+++ publisher.py
@@ -2,7 +2,7 @@ from gossip_node import GossipNode
 import time
 
 if __name__ == "__main__":
-    gossip_node = GossipNode(host="192.168.178.67",port=5000)
+    gossip_node = GossipNode(host="127.0.0.1",port=5000)
     i = 0
     while True:
         gossip_node.publish("Temperature", f"Temperature is {i}°C")
